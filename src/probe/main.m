3527bccd5efce696dbc1529a9490dbc81b418b92
Jake Ireland <jakewilliami@icloud.com>
2023-10-23 01:09:30 +1300

    Do not toggle visibility on already-hidden applications

--- src/probe/main.m
+++ src/probe/main.m
@@ -1,33 +1,48 @@
+// TODO: print hidden file option
+// TODO: refresh hidden file option
+// TODO: persist to tmp
+
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
-    NSWorkspace *workspace = [NSWorkspace sharedWorkspace];
     NSString *configFilePath = [NSHomeDirectory() stringByAppendingPathComponent:@"hidden_apps.json"];
-
     NSError *error;
-    NSMutableSet *hiddenAppIdentifiers = [NSMutableSet set];
 
-    // Read the configuration file to get the list of hidden application process IDs
+    // Read state file
+    NSMutableSet *targetAppIDs = [NSMutableSet set];
     NSData *data = [NSData dataWithContentsOfFile:configFilePath options:0 error:&error];
     if (data) {
-        NSArray *hiddenProcessIDs = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-        [hiddenAppIdentifiers addObjectsFromArray:hiddenProcessIDs];
+        NSArray *mmStateFS = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        [targetAppIDs addObjectsFromArray:mmStateFS];
     }
 
     // Toggle the visibility of applications
-    for (NSRunningApplication *app in workspace.runningApplications) {
-        if ([hiddenAppIdentifiers containsObject:@(app.processIdentifier)]) {
-            [app unhide];
-            [hiddenAppIdentifiers removeObject:@(app.processIdentifier)];
-        } else {
+    for (NSRunningApplication *app in [NSWorkspace sharedWorkspace].runningApplications) {
+        // If the application is active (the first branch of the if statement), then we want to hide it
+        // and add its ID to the list.  This is the first stage of the toggle.  The second stage of the
+        // toggle is a little more complicated (this is the second branch of the if statement).  We need
+        // to check if the hidden application is in the list.  If it is, then we have hidden it with the
+        // present application, and we need to unhide it accordingly.  If it's not in the list, then it
+        // was already hidden, so we can safely ignore it.
+
+        // NSLog(@"Processing application: %@ (%d)", app.localizedName, app.processIdentifier);
+        if (![app isHidden]) {
+            NSLog(@"Hiding active application: %@ (%d)", app.localizedName, app.processIdentifier);
             [app hide];
-            [hiddenAppIdentifiers addObject:@(app.processIdentifier)];
+            [targetAppIDs addObject:@(app.processIdentifier)];
+        } else {
+            NSLog(@"Application already hidden: %@ (%d)", app.localizedName, app.processIdentifier);
+            if ([targetAppIDs containsObject:@(app.processIdentifier)]) {
+                NSLog(@"Target in list: %@ (%d)", app.localizedName, app.processIdentifier);
+                [app unhide];
+                [targetAppIDs removeObject:@(app.processIdentifier)];
+            }
         }
     }
 
-    // Write the updated configuration file with the current hidden application process IDs
-    NSArray *updatedHiddenProcessIDs = [hiddenAppIdentifiers.allObjects valueForKeyPath:@"@unionOfObjects.integerValue"];
-    NSData *updatedData = [NSJSONSerialization dataWithJSONObject:updatedHiddenProcessIDs options:0 error:nil];
+    // Update state file
+    NSArray *newTargetAppIDs = [targetAppIDs.allObjects valueForKeyPath:@"@unionOfObjects.integerValue"];
+    NSData *updatedData = [NSJSONSerialization dataWithJSONObject:newTargetAppIDs options:0 error:nil];
     [updatedData writeToFile:configFilePath atomically:YES];
 
     return 0;
